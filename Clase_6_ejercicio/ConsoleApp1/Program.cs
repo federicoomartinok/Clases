ed728da300b82db8fef1f742b49d68f49e99d347
Federico Martin <federicoomartinok@gmail.com>
2023-02-17 13:25:52 -0300

    Ejercicio de autos y de jugadores

---
+++ Clase_6_ejercicio/ConsoleApp1/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace Ejercicios
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Auto autoFord = new ("MMMM", "blanco", 300, "Ford", "Camaro");
+            Moto motoBmw = new ("MX123","Rojo",270,"Bmw","F600");
+
+            Console.WriteLine("------------AUTO------------");
+
+            Console.WriteLine(autoFord.ToString());
+            autoFord.Arrancar();
+            autoFord.Acelerar();
+            autoFord.Acelerar();
+            autoFord.Acelerar();
+            autoFord.Detener();
+            Console.WriteLine("------------Moto------------");
+
+            Console.WriteLine(motoBmw.ToString());
+            motoBmw.Arrancar();
+            motoBmw.Acelerar();
+            motoBmw.Acelerar();
+            motoBmw.Acelerar();
+            motoBmw.Detener();
+        }
+
+        interface IVehiculo
+        {
+            public void Arrancar();
+            public void Detener();
+            public void Acelerar();
+        }
+
+
+        public abstract class Vehiculo
+        {
+            public string numeroSerie { get; set; }
+            public string color { get; set; }
+            public float velocidadMaxima { get; set; }
+            public string marca { get; set; }
+            public string modelo { get; set; }
+            protected float velocidadActual { get; set; }
+
+            public Vehiculo(string numeroSerie, string color, float velocidadMaxima, string marca
+                , string modelo)
+            {
+                this.numeroSerie = numeroSerie;
+                this.color = color;
+                this.velocidadMaxima = velocidadMaxima;
+                this.marca = marca;
+                this.modelo = modelo;
+            }
+
+            public override string ToString()
+            {
+                return "Numero de serie:" + this.numeroSerie + "\nColor: " + this.color +
+                    "\nVelocidad Maxima: " + this.velocidadMaxima + "\nMarca: " + this.marca +
+                    "\nModelo: " + this.modelo;
+            }
+        }
+
+        class Moto : Vehiculo, IVehiculo
+        {
+            public Moto(string numeroSerie, string color, float velocidadMaxima, string marca, string modelo)
+                : base(numeroSerie, color, velocidadMaxima, marca, modelo)
+            {
+            }
+            public void Acelerar()
+            {
+                this.velocidadActual += 5;
+                Console.WriteLine("Acelerando Moto a: {0} Km/h", this.velocidadActual);
+            }
+
+            public void Arrancar()
+            {
+                Console.WriteLine("Arrancando Moto....En Arranque");
+            }
+
+            public void Detener()
+            {
+                Console.WriteLine("Deteniendo la Moto.... Detenida");
+            }
+
+        }
+
+        class Auto : Vehiculo, IVehiculo
+        {
+            public Auto(string numeroSerie, string color, float velocidadMaxima, string marca,
+                string modelo)
+                : base(numeroSerie, color, velocidadMaxima, marca, modelo)
+            {
+            }
+
+            public void Acelerar()
+            {
+                this.velocidadActual += 10;
+                Console.WriteLine("Acelerando Auto a: {0} Km/h", this.velocidadActual);
+            }
+            public void Arrancar()
+            {
+                Console.WriteLine("Arrancando Auto....En Arranque");
+            }
+
+            public void Detener()
+            {
+                Console.WriteLine("Deteniendo El Auto.... Detenido");
+            }
+        }
+
+    }
+
+}
---
+++ Clase_6_ejercicio/Ejercicio de Jugadores/Ejercicio de Jugadores/Program.cs
@@ -0,0 +1,154 @@
+﻿namespace ejercicioJugadores
+{
+    class ProbarMain
+    {
+        static void Main(string[] args)
+        {
+
+            int regate;       
+            Console.Write("Ingrese con cuanto termino messi el regate: ");
+            regate = int.Parse(Console.ReadLine());
+
+            Messi messi = new Messi(regate, 88, 95, 30, 85, 60);
+            CristianoRonaldo ronaldo = new CristianoRonaldo(99, 88, 98, 21, 83, 62);
+
+            messi.Correr();
+            messi.Rematar();
+            messi.Regatear();
+            messi.Defender();
+            messi.Asistir();
+            messi.Aguantar();
+            Console.WriteLine("");
+            ronaldo.Correr();
+            ronaldo.Rematar();
+            ronaldo.Regatear();
+            ronaldo.Defender();
+            ronaldo.Asistir();
+            ronaldo.Aguantar();
+        }
+
+    
+        interface IJugador
+        {
+            public void Correr();
+            public void Rematar();
+            public void Regatear();
+            public void Defender();
+            public void Asistir();
+            public void Aguantar();
+
+        }
+
+        public abstract class Jugador
+        {
+            public int velocidad { get; set; }
+            public int tiro { get; set; }
+            public int regate { get; set; }
+            public int defensa { get; set; }
+            public int pase { get; set; }
+            public int resistencia { get; set; }
+
+
+            public Jugador(int velocidad, int tiro, int regate, int defensa, int pase, int resistencia)
+            {
+                this.velocidad = velocidad;
+                this.tiro = tiro;
+                this.regate = regate;
+                this.defensa = defensa;
+                this.pase = pase;
+                this.resistencia = resistencia;
+            }               
+
+        }
+
+        class Messi : Jugador, IJugador
+        {
+            public Messi(int velocidad, int tiro, int regate, int defensa, int pase, int resistencia)
+                : base(velocidad,tiro,regate,defensa,pase,resistencia)
+            {
+            }
+
+            public void Correr()
+            {
+                this.velocidad = velocidad;
+                Console.WriteLine("Lionel Messi corrio con {0} de velocidad", this.velocidad);
+            }
+
+            public void Rematar()
+            {
+                this.tiro = tiro;
+                Console.WriteLine("Lionel Messi remató con {0} de remate", this.tiro);
+
+            }
+            public void Regatear() 
+            {
+                this.regate = regate;
+                Console.WriteLine("Lionel Messi regateó con {0} de regate", this.regate);
+            }
+            public void Defender()
+            {
+                this.defensa = defensa;
+                Console.WriteLine("Lionel Messi defendio con {0} de defensa", this.defensa);
+            }
+            public void Asistir()
+            {
+                this.pase = pase;
+                Console.WriteLine("Lionel Messi Asistio con {0}% de efectividad", this.pase);
+
+            }
+            public void Aguantar()
+            {
+                this.resistencia= resistencia;
+                Console.WriteLine("Lionel Messi aguanto con {0} de resistencia", this.resistencia);
+
+            }
+        }
+
+        class CristianoRonaldo : Jugador, IJugador
+        {
+            public CristianoRonaldo(int velocidad, int tiro, int regate, int defensa, int pase, int resistencia)
+                : base(velocidad, tiro, regate, defensa, pase, resistencia)
+            {
+            }
+
+            public void Correr()
+            {
+                this.velocidad = velocidad;
+                Console.WriteLine("Cristiano Ronaldo corrio con {0} de velocidad", this.velocidad);
+            }
+
+            public void Rematar()
+            {
+                this.tiro = tiro;
+                Console.WriteLine("Cristiano Ronaldo remató con {0} de remate", this.tiro);
+
+            }
+            public void Regatear()
+            {
+                this.regate = regate;
+                Console.WriteLine("Cristiano Ronaldo regateó con {0} de regate", this.regate);
+            }
+            public void Defender()
+            {
+                this.defensa = defensa;
+                Console.WriteLine("Cristiano Ronaldo defendio con {0} de defensa", this.defensa);
+            }
+            public void Asistir()
+            {
+                this.pase = pase;
+                Console.WriteLine("Cristiano Ronaldo Asistio con {0}% de efectividad", this.pase);
+
+            }
+            public void Aguantar()
+            {
+                this.resistencia = resistencia;
+                Console.WriteLine("Cristiano Ronaldo aguanto con {0} de resistencia", this.resistencia);
+
+            }
+
+
+        }
+
+        
+    }
+}
