a9134c16ad64f50262757ae49b810f1a1c79845c
Federico Martin <federicoomartinok@gmail.com>
2023-03-07 12:41:26 -0300

    Arrays

--- Listas/Clase 8/Program.cs
+++ Listas/Clase 8/Program.cs
@@ -4,6 +4,20 @@
     {
         static void Main(string[] args)
         {
+            ListasMayores mayores = new ListasMayores();
+            Cliente[] clientes =
+            {
+                new Cliente("Fede",1,"San cristobal",26),
+                new Cliente("Martin",2,"Calle loca",25),
+                new Cliente("Ariel",3,"Av. Godines",22),
+                new Cliente("Facu",4,"Eva peron",44)
+
+            };
+
+           mayores.InsertarEnLista(clientes);
+
+            mayores.MostrarLista();
+
             
         }
 
@@ -33,6 +47,39 @@
             {
                 _clientesMayores = new List<Cliente>();
             }
+
+
+            public void InsertarEnLista(Cliente[] clientes)
+            {
+                Console.WriteLine($"Insertando en lista {clientes.Length} clientes");
+
+                foreach (Cliente cliente in clientes)
+                {
+                    _clientesMayores.Add(cliente);
+                }
+            }
+
+            public bool BorrarCliente(Cliente cliente)
+            {
+                bool seBorroElCliente = false;
+
+                if (_clientesMayores.Contains(cliente))
+                {
+                    seBorroElCliente = _clientesMayores.Remove(cliente);
+                }
+
+                return seBorroElCliente;
+            }
+
+            public void MostrarLista()
+            {
+                foreach (Cliente cliente in _clientesMayores)
+                {
+                    Console.WriteLine($"Nombre: {cliente.Nombre} - ID: {cliente.Id} " +
+                        $"-Direccion: {cliente.Direccion} - Edad: {cliente.Edad}");
+                }
+            }
         }
+        
     }
 }
